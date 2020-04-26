42f4a660d2d6dc2870b9f5de49858843fb63d4eb
komosyu4649 <komosyu4649@gmail.com>
2020-04-26 11:58:21 +0900

    Updates

---
+++ precache-manifest.3865f170d6e740a19cf1af0206e369f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9988c2f2388df2935aba029a6c222eb9",
+    "url": "/origin/index.html"
+  },
+  {
+    "revision": "2b8b9b1ff65a9fffa8e1",
+    "url": "/origin/static/css/main.e2a91871.chunk.css"
+  },
+  {
+    "revision": "91bbd15c9748d289a07b",
+    "url": "/origin/static/js/2.f923e80d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/origin/static/js/2.f923e80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8b9b1ff65a9fffa8e1",
+    "url": "/origin/static/js/main.4f818310.chunk.js"
+  },
+  {
+    "revision": "a942b5e7fd2ecacd9621",
+    "url": "/origin/static/js/runtime-main.5aab3093.js"
+  }
+]);
